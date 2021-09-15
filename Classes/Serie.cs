48ac04f899251367bb4ac371121f8ec746538462
filipeluiz <ffilipeluiz@gmail.com>
2021-09-15 00:22:10 -0300

    Initital commit

---
+++ Classes/Serie.cs
@@ -0,0 +1,10 @@
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+    }
+}
